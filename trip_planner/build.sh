c7cfd2d80112ed0948f13ea507b1b219d08baee3
Makai <danielmakai92@gmail.com>
2025-04-02 11:46:15 +0300

    created a superuser

--- trip_planner/build.sh
+++ trip_planner/build.sh
@@ -3,3 +3,4 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', 'admin123') if not User.objects.filter(username='admin').exists() else None" | python manage.py shell
